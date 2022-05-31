739c1858ada22e5971e9a369fd51705f0910d5bb
kamaltripathi <kamaltri123@gmail.com>
2022-05-31 18:34:53 +0530

    updated

--- 4_array/array.cpp
+++ 4_array/array.cpp
@@ -3,9 +3,8 @@
 
 int main() {
     
-    int arry[10];
-    arry[1] = 2;
-    std::cout << arry << std::endl;    
+    int arry[4] = { 3, 4, 5, 6 };
+    std::cout << arry[0] << std::endl;    
 	
 	
 	
