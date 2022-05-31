da92861e1b86822ad707581eb5cb39f2d2b3f0a6
kamaltripathi <kamaltri123@gmail.com>
2022-05-31 18:10:26 +0530

    init commit

---
+++ 3_variables/variable.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+
+int main() {
+
+    int a = 2;
+    double b = 2.01;
+    char c = 'x';
+    std::string d = "Hello World STRING";
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    return 0;
+}
+
+
